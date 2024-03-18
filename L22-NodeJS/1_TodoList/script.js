eb810d5bb423f18c578e9e4b4a09f643301bc93a
Kartik <kartik.mathur10@gmail.com>
2024-03-18 20:26:56 +0530

    TodoList-jQuery

---
+++ L22-NodeJS/1_TodoList/script.js
@@ -0,0 +1,59 @@
+$('body')
+    .append(
+        $('<h1>').text('Todo List').addClass('heading')
+    )
+    .append(
+        $('<div>')
+            .addClass('inputDetails')
+            .append(
+                $('<input>')
+                    .attr('placeholder', 'Enter New Task')
+                    .attr('type', 'text')
+                    .addClass('newtask')
+            )
+            .append(
+                $('<button>')
+                    .text('Add Task')
+                    .addClass('btn')
+                    .click(ev => {
+                        $('.tasklist')
+                            .append(
+                                $('<li>')
+                                    .addClass('task')
+                                    .text(
+                                        $('.newtask').val()
+                                    )
+                                    .append(
+                                        $('<div>')
+                                            .addClass('btngrp')
+                                            .append(
+                                                $('<button>').text('↑').addClass('upBtn')
+                                            )
+                                            .append(
+                                                $('<button>').text('↓').addClass('downBtn')
+                                            )
+                                            .append(
+                                                $('<button>').text('❌').addClass('deleteBtn')
+                                            )
+                                    )
+                            )
+                    })
+            )
+    )
+    .append(
+        $('<ul>')
+            .addClass('tasklist')
+            .click(ev => {
+                if($(ev.target).hasClass('upBtn')){
+                    $(ev.target).parent().parent()
+                        .insertBefore($(ev.target).parent().parent().prev())
+                }
+                else if($(ev.target).hasClass('downBtn')){
+                    $(ev.target).parent().parent()
+                        .insertAfter($(ev.target).parent().parent().next())
+                }
+                else if($(ev.target).hasClass('deleteBtn')){
+                    $(ev.target).parent().parent().remove()
+                }
+            })
+    )
