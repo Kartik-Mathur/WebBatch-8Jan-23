c5d9508d8d2cb143ada0c7c6c9f7b5f64b45aee8
Kartik <kartik.mathur10@gmail.com>
2024-03-27 20:22:27 +0530

    req-res cycle

---
+++ L25-ExpressJS/app.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/gettask',(req,res)=>{
+    const {task} = req.query;
+    res.send(task);
+})
+
+app.listen(PORT,()=>{
+    console.log(`http://localhost:`+PORT);
+});
