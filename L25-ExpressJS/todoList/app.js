1ee2b52e62eb20b3fb91279b7be280937babb811
Kartik <kartik.mathur10@gmail.com>
2024-03-28 20:29:58 +0530

    TodoList

--- L25-ExpressJS/todoList/app.js
+++ L25-ExpressJS/todoList/app.js
@@ -8,21 +8,49 @@ app.use(express.json());
 
 let todos = ["Dancing", "Singing", "Coding", "Reading"];
 
-app.get('/todos',(req,res)=>{
+app.get('/todos', (req, res) => {
     res.status(200).send(todos);
 })
 
-app.get('/addtask',(req,res)=>{
-    let {task} = req.query;
+app.get('/addtask', (req, res) => {
+    let { task } = req.query;
     // Agar task already hai toh don't add it to the array
-    if(!todos.includes(task)){
+    if (!todos.includes(task)) {
         todos.push(task);
     }
-    
+
     res.redirect('/todos');
 })
 
-app.use((req,res,next)=>{
+app.get('/increase', (req, res) => {
+    const {task} = req.query;
+    let indx = todos.indexOf(task);
+    if(indx>0 && indx<=todos.length-1){
+        let temp = todos[indx];
+        todos[indx] = todos[indx-1];
+        todos[indx-1] = temp;
+    }
+    res.redirect('/todos');
+})
+
+app.get('/decrease', (req, res) => {
+    const {task} = req.query;
+    let indx = todos.indexOf(task);
+    if(indx>=0 && indx<todos.length-1){
+        let temp = todos[indx];
+        todos[indx] = todos[indx+1];
+        todos[indx+1] = temp;
+    }
+    res.redirect('/todos');
+})
+
+app.get('/delete', (req, res) => {
+    const {task} = req.query;
+    todos = todos.filter(d=>d!=task);
+    res.redirect('/todos');
+})
+
+app.use((req, res, next) => {
     console.log("Yaha par hai")
     res.status(404).json({
         msg: "Kya Ulti Seedhi Req Bhej raha hai mujhe",
--- L25-ExpressJS/todoList/public/script.js
+++ L25-ExpressJS/todoList/public/script.js
@@ -7,7 +7,13 @@ function updateTaskList(todos){
     taskList.innerText = ""
     todos.forEach(e=>{
         let li = document.createElement('li');
-        li.innerText = e;
+        li.classList.add('item');
+        li.innerHTML = `<div class="content">${e}</div>
+        <div class="btnGroup">
+            <button class="btn-1 upBtn">↑</button>
+            <button class="btn-1 downBtn">↓</button>
+            <button class="btn-1 deleteBtn">❌</button>
+        </div>`;
         taskList.appendChild(li);
     })
 }
@@ -33,3 +39,22 @@ async function initialTodosLoad(){
     }
 }
 initialTodosLoad()
+
+
+
+// Implementation of Increase, Decrease and Delete Functionality
+taskList.addEventListener('click',async (ev)=>{
+    let item = ev.target;
+    if(item.classList.contains('upBtn')){
+        let {data} = await axios.get(`/increase?task=${item.parentElement.previousElementSibling.innerText}`)
+        updateTaskList(data);
+    }
+    else if(item.classList.contains('downBtn')){
+        let {data} = await axios.get(`/decrease?task=${item.parentElement.previousElementSibling.innerText}`)
+        updateTaskList(data);
+    }
+    else if(item.classList.contains('deleteBtn')){
+        let {data} = await axios.get(`/delete?task=${item.parentElement.previousElementSibling.innerText}`)
+        updateTaskList(data);
+    }
+})
