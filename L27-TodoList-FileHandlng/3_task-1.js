4012f927e9eac9a1977e7be1e60d6e2f4731ac88
Kartik <kartik.mathur10@gmail.com>
2024-04-01 21:42:10 +0530

    Learning APIs and Post Request

--- L27-TodoList-FileHandlng/3_task-1.js
+++ L27-TodoList-FileHandlng/3_task-1.js
@@ -1,6 +1,17 @@
+let students = [
+    "Abhishek Pandey", "Archana", "Varda", "Ishan Gupta"
+];
+
+let teachers = [
+    "Kartik", "Monu", "Sabeel", "Mosina"
+];
+
 // IMPLEMENT THE FOLLOWING APIs
 // 1. GET '/students': GET Details of all students
 // 2. POST '/student': Add one student to students[]
 // 3. POST '/student/remove': Remove the student by name provided in queryParam
 // 4. GET 'teachers': GET details of all teachers
 // 5. POST '/teachers/update': Update the name of the teacher using body {oldName, newName}
+// For example: body : {oldName: "Kartik", newName: "Kartik Mathur"}
+// ["Kartik", "Monu", "Sabeel", "Mosina"]
+// ["Kartik Mathur", "Monu", "Sabeel", "Mosina"]
