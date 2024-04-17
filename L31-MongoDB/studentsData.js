bb6ef7cea6061a4c07834c85dd59acc4ead3f37d
Kartik <kartik.mathur10@gmail.com>
2024-04-17 20:41:10 +0530

    MongoDB

---
+++ L31-MongoDB/studentsData.js
@@ -0,0 +1,20 @@
+let students = [
+    {
+        name: 'Chandan',
+        age: 28,
+        subject: 'C++',
+        marks: 90
+    },
+    {
+        name: 'Himanshu',
+        age: 20,
+        subject: 'OS',
+        marks: 70
+    },
+    {
+        name: 'Harsh',
+        age: 22,
+        subject: 'CP',
+        marks: 90
+    }
+]
