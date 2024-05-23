55d17ca6fbcddda6fa277581c27ae5b04f2ddd99
Kartik <kartik.mathur10@gmail.com>
2024-05-23 21:37:18 +0530

    Chat App

--- L40-Sockets/app.js
+++ L40-Sockets/app.js
@@ -11,9 +11,32 @@ app.set('view engine', 'hbs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// emit : Se event ko send krte hai
+// on: Se event ko listen krte hai
+
+let userMap = {};
 
 io.on("connection", (socket) => {
-    // ...
+    console.log(socket.id);
+    socket.on('newuser',({socketId,name})=>{    
+        userMap[socket.id] = name;
+
+        socket.emit('useradded',{
+            msg: "User added successfully",
+            username: userMap[socket.id]
+        });
+    })
+    
+    socket.on('newmessage',({message,socketId})=>{
+        console.log(message,socketId);
+        io.emit('messagerecieved',{
+            message,
+            username: userMap[socketId],
+            socketId: socket.id
+        })
+    })
+
+
 });
 
 httpServer.listen(PORT, () => {
---
+++ L40-Sockets/public/js/script.js
@@ -0,0 +1,57 @@
+let socket = io();
+console.log(socket);
+let login = document.querySelector('.login');
+let chatApplication = document.querySelector('.chat-application');
+
+document.querySelector('.login-btn').addEventListener('click',(ev)=>{
+    let username = document.querySelector('.username').value;
+    console.log(username)
+    if(username.length>0){
+        socket.emit('newuser',{
+            socketId: socket.id,
+            name: username
+        })
+    }
+    else{
+        alert('Please enter correct username');
+    }
+})
+
+socket.on('useradded',({msg,username})=>{
+    console.log(msg);
+    login.style.display = 'none';
+    chatApplication.style.display = 'block';
+    let currentUser = document.querySelector('.current-user');
+    currentUser.innerText = username;
+})
+
+document.querySelector('.send-button').addEventListener('click',()=>{
+    let messageInput = document.querySelector('.message-input');
+    let message = messageInput.value;
+    if(message.length>0){
+        messageInput.value = '';
+        socket.emit('newmessage',{
+            message: message,
+            socketId: socket.id
+        })
+    }
+})
+
+socket.on('messagerecieved',({message,socketId,username})=>{
+    let chats = document.querySelector('.chats');
+    let chat = document.createElement('div');
+    chat.classList.add('chat');
+    // chat.innerHTML = `<div class="chat-message">${message}</div>`;
+    let chatMessage = document.createElement('div');
+    chatMessage.classList.add('chat-message');
+    chatMessage.innerText = message;
+   
+    if(socketId === socket.id){
+        chatMessage.classList.add('my-chat');
+    }
+    else{
+        chatMessage.classList.add('another-chat');
+    }
+    chat.appendChild(chatMessage);
+    chats.appendChild(chat);
+})
